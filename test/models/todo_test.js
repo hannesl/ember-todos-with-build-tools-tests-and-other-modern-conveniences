fb7ecb3ed4a1dcca4f7c92fbb67f51a61e607c4c
Hannes Lilljequist <hannes@sthlmconnection.se>
2013-02-07 10:03:36 +0100

    Made most tests pass with Ember Data.

--- test/models/todo_test.js
+++ test/models/todo_test.js
@@ -1,45 +1,45 @@
 test("a Todo begins with completed set to false", function(){
-  var todo = Todos.Todo.create();
+  var todo = Todos.Todo.createRecord();
   equal(todo.get('completed'), false);
 });
 
 test("a Todo has access to the localStore store", function(){
-  var todo = Todos.Todo.create();
-  ok(todo.get('store') instanceof Todos.Store, "todo's store is a Store");
+  var todo = Todos.Todo.createRecord();
+  ok(todo.store instanceof DS.Store, "todo's store is a Store");
 });
 
 test("when a Todo's title changes it automatically saves", function(){
-  var todo = Todos.Todo.create();
-  var update = sinon.stub(todo.get('store'), 'update');
+  var todo = Todos.Todo.createRecord();
+  var recordAttributeDidChange = sinon.stub(todo.store, 'recordAttributeDidChange');
 
   Ember.run(function(){
     todo.set('title', 'a new title');
   });
 
-  ok(update.calledOnce);
+  ok(recordAttributeDidChange.calledOnce);
 
-  update.restore();
+  recordAttributeDidChange.restore();
 });
 
 test("when a Todo's completed status changes it automatically saves", function(){
-  var todo = Todos.Todo.create();
-  var update = sinon.stub(todo.get('store'), 'update');
+  var todo = Todos.Todo.createRecord();
+  var recordAttributeDidChange = sinon.stub(todo.store, 'recordAttributeDidChange');
 
   Ember.run(function(){
     todo.toggleProperty('completed');
   });
 
-  ok(update.calledOnce);
+  ok(recordAttributeDidChange.calledOnce);
 
-  update.restore();
+  recordAttributeDidChange.restore();
 });
 
-test("the Todo constructor has access to the store", function(){
-  ok(Todos.Todo.store instanceof Todos.Store, "Todo store is a Store");
+test("the Todos application has access to the store", function(){
+  ok(Todos.store instanceof DS.Store, "Todo store is a Store");
 });
 
 test("creating a Todo proxies to the store", function(){
-  var create = sinon.stub(Todos.Todo.store, 'createRecord'),
+  var create = sinon.stub(Todos.store, 'createRecord'),
       properties = {title: 'hi'};
   Todos.Todo.createRecord(properties);
 
@@ -48,16 +48,18 @@ test("creating a Todo proxies to the store", function(){
 });
 
 test("destroying a Todo proxies to the store", function(){
-  var destroy = sinon.stub(Todos.Todo.store, 'destroy'),
-      todo = Todos.Todo.createRecord();
 
-  Todos.Todo.destroy(todo);
-  ok(destroy.calledOnce);
-  destroy.restore();
+  var deleteRecord = sinon.stub(Todos.store, 'deleteRecord'),
+      todo = Todos.Todo.createRecord({title: 'hi'});
+
+  todo.deleteRecord(todo);
+  ok(deleteRecord.calledOnce);
+  deleteRecord.restore();
+
 });
 
 test("asking for all todos proxies to the store", function(){
-  var all = sinon.stub(Todos.Todo.store, 'all');
+  var all = sinon.stub(Todos.store, 'all');
   Todos.Todo.all();
 
   ok(all.calledOnce);
